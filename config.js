b039fb99b017a6b9af07bd0025f7d3f9e25572e2
ontorislam6996-lgtm <ontorislam6996@gmail.com>
2025-11-15 17:19:41 +0600

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "your-bot-token-here", // Add your bot's token here (REQUIRED)
-    channel_name: "@devuuuu_xd", // Specify your YouTube channel's name here (REQUIRED)
+    token: "MTM4NTMyNTg1NjY3ODc0MDAxOQ.GBr0Wb.xOf6PuLUukPjmjBLivVh0pq9RbKUGZ_VtGdrPs", // Add your bot's token here (REQUIRED)
+    channel_name: "@CYBERXCHEAT", // Specify your YouTube channel's name here (REQUIRED)
     role_id: "", // ID of the role you want to assign (OPTIONAL)
     keywords: "subscribe, follow, like", // Specify the keywords separated by commas (OPTIONAL)
     save_data: "true" // Save data in subscriber.json (OPTIONAL)
